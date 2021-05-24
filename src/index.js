1a16606a27730eec8b029eba65709d05f58365ec
dibdas <dibyandu08das@gmail.com>
2021-05-24 17:01:20 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
 import CreditsScene from './Scenes/CreditsScene';
+import Model from './Model';
 
 class Game extends Phaser.Game {
   constructor () {
     super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
@@ -17,7 +20,6 @@ class Game extends Phaser.Game {
     this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
     this.scene.start('Boot');
-    
   }
 }
 
