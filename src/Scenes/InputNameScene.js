1ccca51ecd71c1e2de4eb868afe1eb213623d297
dibdas <dibyandu08das@gmail.com>
2021-05-27 01:12:57 +0530

    added game

--- src/Scenes/InputNameScene.js
+++ src/Scenes/InputNameScene.js
@@ -1,27 +1,28 @@
 import 'phaser';
 import config from '../Config/config';
+import Button from '../Objects/Button';
 
 export default class InputNameScene extends Phaser.Scene {
     constructor(){
     super('InputName');
     }
-    preload(){
-
-    }
+    
     create(){
-
-    this.message = this.add.text(640, 250, "Hello, --", {
-        color: "#FFFFFF",
-        fontSize: 60,
-        fontStyle: "bold"
-    }).setOrigin(0.5);
-    this.input = this.add.dom(200, 235, 'input', {
-        type: 'text',
-        name: 'name',
-        fontSize: '30px',
-        backgroundColor: '#fff',
-      });
-
+        this.gameButton = new Button(this, config.scale.width/2, config.scale.height/2 - 100, 'blueButton1', 'blueButton2', 'Start', 'Game');
     }
+
+  centerButton (gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(config.scale.width/2, config.scale.height/2 - offset * 100, config.scale.width, config.scale.height)
+    );
+  }
+
+  centerButtonText (gameText, gameButton) {
+    Phaser.Display.Align.In.Center(
+      gameText,
+      gameButton
+    );
+  }
 
 }
