4f59735834f95a2deba2b0076fb5beab6604297f
dibdas <dibyandu08das@gmail.com>
2021-05-27 00:29:45 +0530

    added explode function

--- src/Objects/Entities.js
+++ src/Objects/Entities.js
@@ -3,6 +3,7 @@ import config from '../Config/config';
 
 class Entity extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, key, type) {
+        super(scene, x, y, key);
 
 this.scene = scene;
 this.scene.add.existing(this);
@@ -12,19 +13,32 @@ this.setData("isDead", false);
 
     }
 
-  class PlayerLaser extends Entity {
-    constructor(scene, x, y) {
-      super(scene, x, y, 'sprLaserPlayer');
-      this.body.velocity.y = -200;
-    }
-  }
-class EnemyLaser extends Entity {
-    constructor(scene, x, y) {
-      super(scene, x, y, "sprLaserEnemy0");
-      this.body.velocity.y = 200;
+  explode(canDestroy) {
+    if (!this.getData("isDead")) {
+      // Set the texture to the explosion image, then play the animation
+      this.setTexture("sprExplosion");  // this refers to the same animation key we used when we added this.anims.create previously
+      this.play("sprExplosion"); // play the animation
+      // pick a random explosion sound within the array we defined in this.sfx in SceneMain
+      this.scene.sfx.explosions[Phaser.Math.Between(0, this.scene.sfx.explosions.length - 1)].play();
+      if (this.shootTimer !== undefined) {
+        if (this.shootTimer) {
+          this.shootTimer.remove(false);
+        }
+      }
+      this.setAngle(0);
+      this.body.setVelocity(0, 0);
+      this.on('animationcomplete', function() {
+        if (canDestroy) {
+          this.destroy();
+        }
+        else {
+          this.setVisible(false);
+        }
+      }, this);
+      this.setData("isDead", true);
     }
   }
   
 
-  
 }
+export default Entity;
