0e89147e16b4fa2723053572e242a2d0e5768b8a
dibdas <dibyandu08das@gmail.com>
2021-05-27 00:57:03 +0530

    added playerLaser

---
+++ src/Objects/PlayerLaser.js
@@ -0,0 +1,13 @@
+
+import 'phaser';
+import Entity from './Entities';
+
+
+class PlayerLaser extends Entity {
+    constructor(scene, x, y) {
+      super(scene, x, y, 'sprLaserPlayer');
+      this.body.velocity.y = -200;
+    }
+  }
+
+  export default PlayerLaser;
