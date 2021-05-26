492ee6f93dc40484fd8a7d3c65a2985ad5914cc4
dibdas <dibyandu08das@gmail.com>
2021-05-26 14:36:16 +0530

    added carriership

---
+++ src/Objects/ChaserShip.js
@@ -0,0 +1,9 @@
+import Phaser from 'phaser';
+import Entity from './Entities';
+
+class ChaserShip extends Entity {
+    constructor(scene, x, y) {
+      super(scene, x, y, "sprEnemy1", "ChaserShip");
+      this.body.velocity.y = Phaser.Math.Between(50, 100);
+    }
+  }
