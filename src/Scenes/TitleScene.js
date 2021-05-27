6d8b56edd5bc579bb46073358a063f5c98724d5d
dibdas <dibyandu08das@gmail.com>
2021-05-28 03:00:11 +0530

    modified

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,40 +1,48 @@
-import 'phaser'
-import config from '../Config/config'
-import Button from '../Objects/Button'
+import 'phaser';
+import config from '../Config/config';
+import Button from '../Objects/Button';
+import Form from '../Objects/PlayerForm';
+import '../../assets/css/style.css';
 
-export default class TitleScene extends Phaser.Scene {
-    constructor () {
-        super('Title')
-    }
+export default class TitleScene extends Phaser.Scene { // eslint-disable-line 
+  constructor() {
+    super('Title');
+  }
 
-    create () {
+  create() {
+    Form.showForm();
+
     // Game
-        this.gameButton = new Button(this, config.scale.width/2, config.scale.height/2 - 100, 'blueButton1', 'blueButton2', 'Play', 'InputName')
+    this.gameButton = new Button(this, config.scale.width / 2, config.scale.height / 2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
+
+    // Credits
+    this.creditsButton = new Button(this, config.scale.width / 2, config.scale.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
 
 
-        // Credits
-        this.creditsButton = new Button(this, config.scale.width/2, config.scale.height/2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits')
+    this.leaderboardButton = new Button(this, config.scale.width / 2, config.scale.height / 2 + 200, 'blueButton1', 'blueButton2', 'Board', 'Leaderboard');
 
-        this.model = this.sys.game.globals.model
-        if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-            this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true })
-            this.bgMusic.play()
-            this.model.bgMusicPlaying = true
-            this.sys.game.globals.bgMusic = this.bgMusic
-        }
+
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
+      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.model.bgMusicPlaying = true;
+      this.sys.game.globals.bgMusic = this.bgMusic;
     }
+  }
 
-    centerButton (gameObject, offset = 0) {
-        Phaser.Display.Align.In.Center(
-            gameObject,
-            this.add.zone(config.scale.width/2, config.scale.height/2 - offset * 100, config.scale.width, config.scale.height)
-        )
-    }
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center( // eslint-disable-line 
+      gameObject, // eslint-disable-line 
+      this.add.zone(config.scale.width / 2, config.scale.height / 2 - offset * 100,
+        config.scale.width, config.scale.height),
+    );
+  }
 
-    centerButtonText (gameText, gameButton) {
-        Phaser.Display.Align.In.Center(
-            gameText,
-            gameButton
-        )
-    }
+  centerButtonText(gameText, gameButton) { // eslint-disable-line 
+    Phaser.Display.Align.In.Center( // eslint-disable-line 
+      gameText,
+      gameButton,
+    );
+  }
 }
